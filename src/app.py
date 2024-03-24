89ee812d8af7a06f871c62ecdb931eb304924339
Kranthi Kiran GV <kranthi.gv@gmail.com>
2024-03-23 20:19:55 -0400

    Get mentor value

--- src/app.py
+++ src/app.py
@@ -1,24 +1,28 @@
-from modal import Stub, asgi_app
-from fastapi import FastAPI, Response
+import os
+from modal import Stub, asgi_app, Secret
+from fastapi import FastAPI
 from db import Mentor, Database
 
-stub = Stub('mentorAI')
+stub = Stub('mentorAI', secrets=[Secret.from_name("mentorai")])
 
 web_app = FastAPI()
 db = Database()
-
-
 
 @web_app.get("/")
 def default():
     return "Oops! You are lost."
 
-
 @web_app.get('/mentors')
 def get_mentors() -> list[Mentor]:
     return db.get_mentors()
 
+@web_app.get('/mentors/{mentor_id}')
+def get_mentor(mentor_id: str) -> Mentor:
+    return db.get_mentor(mentor_id)
 
+@web_app.post("/chat")
+def chat():
+    return os.environ["ANTHROPIC_API_KEY"]
 
 @stub.function()
 @asgi_app()
