2f63a8ff79496ee60f3d07ae6e2d2e4acf9c7887
Kranthi Kiran GV <kranthi.gv@gmail.com>
2024-03-24 09:07:12 -0400

    Working roleplay

--- src/app.py
+++ src/app.py
@@ -1,11 +1,30 @@
 import os
-from modal import Stub, asgi_app, Secret
-from fastapi import FastAPI
-from db import Mentor, Database
+from modal import Stub, asgi_app, Secret, Image
+from fastapi import FastAPI, Request
+from db import Mentor, Message, Database
+from anthropic import Anthropic
+from fastapi.middleware.cors import CORSMiddleware
 
-stub = Stub('mentorAI', secrets=[Secret.from_name("mentorai")])
+image = (
+    Image.debian_slim()
+    .pip_install(
+        "anthropic",
+    )
+)
+
+
+stub = Stub('mentorAI', image=image, secrets=[Secret.from_name("mentorai")])
 
 web_app = FastAPI()
+web_app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
 db = Database()
 
 @web_app.get("/")
@@ -20,9 +39,30 @@ def get_mentors() -> list[Mentor]:
 def get_mentor(mentor_id: str) -> Mentor:
     return db.mentors.get(mentor_id)
 
-@web_app.get("/chat/{mentor_id}")
-def chat(mentor_id: str):
-    return (mentor_id, os.environ["ANTHROPIC_API_KEY"])
+@web_app.post("/chat")
+async def chat(request: Request):
+    body = await request.json()
+    mentor_id = body['mentor_id']
+    mentor_name = db.mentors.get(mentor_id).name
+    question = body['msg']
+    API_KEY = os.environ["ANTHROPIC_API_KEY"]
+
+    client = Anthropic(api_key=API_KEY)
+
+    message = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": f"Hello, Claude. You are an expert role playing AI. You are now {mentor_name}. I have a question for you. {question}",
+        }
+    ],
+    model="claude-3-opus-20240229",
+    )
+
+
+
+    return message.content
 
 @stub.function()
 @asgi_app()
--- src/db.py
+++ src/db.py
@@ -5,14 +5,16 @@ class Mentor(BaseModel):
     name: str
     photo: str
 
-
+class Message(BaseModel):
+    mentor_id: str
+    msg: str
 
 
 class Database:
     def __init__(self):
         self.mentors = {
-            'h9ghxb2': Mentor(id='h9ghxb2', name='John Doe', photo='https://example.com/john-doe.jpg'),
-            'p2gb7f0': Mentor(id='p2gb7f0', name='Jane Bpi', photo='https://example.com/jane-doe.jpg'),
+            'h9ghxb2': Mentor(id='h9ghxb2', name='Elon Musk', photo='https://example.com/john-doe.jpg'),
+            'p2gb7f0': Mentor(id='p2gb7f0', name='Taylor Swift', photo='https://example.com/jane-doe.jpg'),
         }
 
     def get_mentors(self) -> list[Mentor]:
